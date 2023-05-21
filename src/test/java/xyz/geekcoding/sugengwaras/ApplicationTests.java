6269581a757f5f492de4c79f54146ad9fd275b6f
idsoke <idrisnotebook@gmail.com>
2023-05-21 21:10:12 +0700

    inisiate project open source aplikasi poliklinik sugeng waras

---
+++ src/test/java/xyz/geekcoding/sugengwaras/ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.geekcoding.sugengwaras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
